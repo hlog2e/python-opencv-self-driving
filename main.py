ce0848781fa8bed592f2061e3353ba8cb0eac2f0
김홍록 | HongRok <dev@hongrok.net>
2022-11-25 19:17:29 +0900

    라이다센서 거리측정 python3 로직으로 변경

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ while(True):
         if count > 8:
             recv = ser.read(9)
             ser.reset_input_buffer()
-            if recv[0] == 'Y' and recv[1] == 'Y':  # 0x59 is 'Y'
-                low = int(recv[2].encode('hex'), 16)
-                high = int(recv[3].encode('hex'), 16)
-                distance = low + high * 256
-                print(distance)
+            if recv[0] == 0x59 and recv[1] == 0x59:  # python3
+                distance = recv[2] + recv[3] * 256
+                strength = recv[4] + recv[5] * 256
+                print('(', distance, ',', strength, ')')
+                ser.reset_input_buffer()
     # ----------------------------------
 
     # 웹캠으로 부터 실시간으로 캡쳐
